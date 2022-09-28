69634109432ad85f92aba61882daf32f6ffa60a7
Joe <uehtung@gmail.com>
2022-09-27 21:12:44 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import { useState } from "react";
 
 function App() {
-  const [isSentence, setIsSentence] = useState(true)
-  const [character, setCharacter] = useState("")
-  const [textInput, setTextInput] = useState("")
-  const [medianCount, setMedianCount] = useState(0)
+  const [isSentence, setIsSentence] = useState(true);
+  const [character, setCharacter] = useState("");
+  const [textInput, setTextInput] = useState("");
+  const [medianCount, setMedianCount] = useState(0);
 
   const splitDoc = (str, isSentence) => {
     if (isSentence) return str.split(/\r?\n|[.]/);
     return str.split(/\r?\n/);
   };
-  
+
   const splitIntoWords = (str) => {
     return str.split(" ");
   };
-  
+
   const charCountFromArr = (arr, a) => {
     const tempArr = arr.filter((item) => item.includes(a));
     return tempArr.length;
   };
-  
+
   function median(arr) {
     const half = Math.floor(arr.length / 2);
     arr.sort((a, b) => a - b);
     if (arr.length % 2) return arr[half];
     return (arr[half - 1] + arr[half]) / 2;
   }
-  
+
   const findMedianCount = (inputDoc, a, sentence) => {
     const resultArr = [];
     const countUnit = splitDoc(inputDoc, sentence);
@@ -37,34 +37,52 @@ function App() {
         resultArr.push(count);
       }
     });
-    return median(resultArr)
-  };  
+    return median(resultArr);
+  };
 
   const handleTextChange = (e) => {
-    setTextInput(e.target.value)
-  }
+    setTextInput(e.target.value);
+  };
 
   const handleRangeChange = () => {
-    setIsSentence(prevState => !prevState)
-  }
+    setIsSentence((prevState) => !prevState);
+  };
 
   const handleCharacterChange = (e) => {
-    setCharacter(e.target.value)
-  }
+    setCharacter(e.target.value);
+  };
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const newMedianCount = findMedianCount(textInput, character, isSentence)
-    setMedianCount(newMedianCount)
-  }
+    e.preventDefault();
+    const newMedianCount = findMedianCount(textInput, character, isSentence);
+    setMedianCount(newMedianCount);
+  };
 
   return (
     <div className="App">
-      <form onSubmit={(e)=>handleSubmit(e)}>
-        <textarea rows={5} onChange={e => handleTextChange(e)} required></textarea>
-        <label htmlFor="character">Target Character</label>
-        <input name="character" type="text" onChange={(e) => handleCharacterChange(e)} required></input>
-        <label htmlFor="isSentence">Check on sentence?</label>
-        <input name="isSentence" type="checkbox" defaultChecked={isSentence} onChange={() => handleRangeChange()}></input>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <textarea
+          rows={5}
+          onChange={(e) => handleTextChange(e)}
+          required
+        ></textarea>
+        <label htmlFor="character">
+          Target Character
+          <input
+            name="character"
+            type="text"
+            onChange={(e) => handleCharacterChange(e)}
+            required
+          ></input>
+        </label>
+        <label htmlFor="isSentence">
+          Check on sentence?
+          <input
+            name="isSentence"
+            type="checkbox"
+            defaultChecked={isSentence}
+            onChange={() => handleRangeChange()}
+          ></input>
+        </label>
         <button type="submit">Check</button>
       </form>
       <div>The Median Count is: {medianCount}</div>
